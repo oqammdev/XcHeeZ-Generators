59515df9fd7dcd056d47703c2d5ff8054041a033
XcHeeZ <71387787+XcHeeZ55@users.noreply.github.com>
2021-02-19 13:45:09 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,7 +1,3 @@
 module.exports = (Discord, client) =>{
     console.log("Activated".yellow)
-setInterval(async function(){
-const code = await client.generate.nitro()
-client.channels.resolve("812285030264602644").send(`https://discord.gift/${code}`)
-}, 1)
 }
