e03f188b1a1e2a5210cfbd3d048503cecb681518
XcHeeZ <71387787+XcHeeZ55@users.noreply.github.com>
2021-02-21 21:53:49 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,7 +1,63 @@
+const request = require("request")
+var proxyURL = "";
+var working = [];
+const ms = require("ms")
+var toMatch;
+
 module.exports = (Discord, client) =>{
     console.log("Activated".yellow)
 	 setInterval(async function(){
 const code = await client.generate.nitro()
-client.channels.resolve("812285030264602644").send(`https://discord.gift/${code}`)
+const ch = client.channels.resolve("812285030264602644")
+
+
+checkNitroCode(code, ch)
+ch.send(`https://discord.gift/${code}`)
+
+
+
+async function checkNitroCode(code = "", channel){
+
+    
+    var proxiedRequest = request.defaults({
+        'proxy': proxyURL
+    });
+    proxiedRequest.timeout = 1500;
+    proxiedRequest.get(`https://discordapp.com/api/v6/entitlements/gift-codes/${code}?with_application=false&with_subscription_plan=true`, (error, resp, body) => {
+        if (error) {
+            console.log("Invaild proxy switching now...")
+
+            
+            
+            return;
+        }
+        try {
+            if (body.code == 200) {
+                const embedS = new MessageEmbed()
+                .setAuthor(`${code} is Working!`)
+                .setColor("GREEN")
+                channel.send("@everyone", embedS)
+
+                msg.edit(embedS)
+                console.log(JSON.stringify(body, null, 4));
+                working.push(`https://discord.gift/${code}`);
+                fs.writeFileSync(__dirname + '/codes.json', JSON.stringify(working, null, 4));
+                if (toMatch === 0) {
+                    process.exit();
+                } else {
+                    //console.log("test")
+                }
+
+            } else {
+return;
+            }
+        } catch (error) {
+            
+
+            console.log("There is an Error!")
+            return;
+        }
+    });
+}
 }, require("ms")("2s"))
 }
