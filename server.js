06cf7d2dcc27b8a15120b087710cbe6592f259cd
XcHeeZ <akhgakamirb@gmail.com>
2021-04-20 23:39:48 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const app = require('express')();
+function generateNitro(){
+    let code = "";
+    let dict = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for (var i = 0; i < 16; i++) {
+        code = code + dict.charAt(Math.floor(Math.random() * dict.length));
+    }
+    return code;
+
+
+}
+
+app.get("/", async (request, response) =>{
+let code = await generateNitro()
+response.send(code)
+})
